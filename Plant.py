7d9631fdef1014468bad2318ce355f6957a1bc96
beansandyaks <gitbinayakmaharjan@gmail.com>
2023-01-09 12:59:31 +0545

    Store Aspect Ratios and Labels in np array

--- Plant.py
+++ Plant.py
@@ -8,12 +8,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
 path = 'Plant_Images'
-files=os.listdir(path)
-images=[]
-labels=[]
-aspect_ratios=[]
-variable=1
-var_=1
+files = os.listdir(path)
+images = []
+labels = []
+aspect_ratios = []
+final_labels = []
+variable = 1
+var_ = 1
 
 for filename in files:
     if filename.endswith(('.JPG', '.jpg')):
@@ -33,11 +34,11 @@ for filename in files:
         mask = cv.inRange(image_hsv, lower_green, upper_green)
 
         # Output after masking
-        output_hsv = image_hsv.copy()
-        output_hsv[np.where(mask == 0)] = 0
+        # output_hsv = image_hsv.copy()
+        # output_hsv[np.where(mask == 0)] = 0
 
         # Conversion to grayscale
-        gray = cv.cvtColor(output_hsv, cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
 
         # Histogram Equalization
         equal_histogram = cv.equalizeHist(gray)
@@ -59,48 +60,60 @@ for filename in files:
             # Finding the bounding box of the leaf
             x, y, w, h = cv.boundingRect(binary)
 
-            # Drawing the bounding reactangle on the image
+            # Drawing the bounding rectangle on the image
             cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # Calculate the aspect ratio of the leaf
             a_ratio = float(w) / h
 
             print("Aspect ratio", a_ratio)
-            aspect_ratios.append(a_ratio)
+            aspect_ratios = np.array([a_ratio])
 
-        file_name = "D:\AI_CW\demo\leaves\leaf_" + str(variable) + ".jpg"
+        file_name = "D://AI_CW//demo//leaves//leaf_" + str(variable) + ".jpg"
         print(file_name)
         variable += 1
         cv.imwrite(file_name, image)
-
-
-x = images
-y = aspect_ratios
 
 print(labels)
 d1 = {'mango': 1, 'basil': 2, 'chinar': 3}
 digits = []
 for label in labels:
     digit = int(label[:4])
-
     digits.append(digit)
-
 print(digits)
+
+
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val][0]
+
+
+for i in digits:
+    final_labels = np.array([(get_keys_from_value(d1, i))])
 print(len(digits))
+print(final_labels)
 
-#Split data into Training and testing sets
-#X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7)
+# Defining the x and y values
 
-#en(X_train),len(X_test)
+final_labels = final_labels.reshape(-1, 1)
+aspect_ratios = aspect_ratios.reshape(-1, 1)
+y = final_labels
+x = aspect_ratios
 
-#clf = RandomForestClassifier(random_state=1)
-#print(clf)
 
-#clf.fit(X_train,y_train)
+# Split data into Training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(x, y, train_size = 0.7)
+
+
+len(X_train), len(X_test)
+
+clf = RandomForestClassifier(random_state=1)
+print(clf)
+
+clf.fit(X_train, y_train)
 
 # Make predictions on the test data
-#predictions = model.predict(X_test)
+predictions = clf.predict(X_test)
 
 # Evaluate the model
-#accuracy = model.score(X_test, y_test)
-#print("Accuracy:", accuracy)
+accuracy = clf.score(X_test, y_test)
+print("Accuracy:", accuracy)
